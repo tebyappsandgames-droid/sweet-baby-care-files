cad2776ead1cf758607548393c97240d43541994
tebyappsandgames-droid <tebyappsandgames@gmail.com>
2026-04-09 12:45:35 -0300

    Update dress_up_screen.dart

--- lib/screens/dress_up_screen.dart
+++ lib/screens/dress_up_screen.dart
@@ -8,33 +8,39 @@ class DressUpScreen extends StatefulWidget {
 }
 
 class _DressUpScreenState extends State<DressUpScreen> {
+  String currentShirt = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/backgrounds/dressing_room_background.png'),
-            fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          Image.asset('assets/backgrounds/dressing_room_background.png', fit: BoxFit.cover, width: double.infinity, height: double.infinity),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Image.asset('assets/characters/hope_ferrer.png', height: 300),
+                    if (currentShirt.isNotEmpty) Positioned(top: 100, child: Image.asset(currentShirt, width: 120)),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(onTap: () => setState(() => currentShirt = 'assets/shirts/blue_shirt.png'), child: Image.asset('assets/shirts/blue_shirt.png', width: 80)),
+                    const SizedBox(width: 20),
+                    GestureDetector(onTap: () => setState(() => currentShirt = 'assets/shirts/green_shirt.png'), child: Image.asset('assets/shirts/green_shirt.png', width: 80)),
+                  ],
+                )
+              ],
+            ),
           ),
-        ),
-        child: Column(
-          children: [
-            const SizedBox(height: 50),
-            Expanded(child: Image.asset('assets/characters/hope_ferrer.png')),
-            Container(
-              height: 150,
-              color: Colors.white.withOpacity(0.5),
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: const [
-                  Padding(padding: EdgeInsets.all(8), child: Icon(Icons.Checkroom, size: 80)),
-                  // Map your assets/shirts and assets/hats here
-                ],
-              ),
-            )
-          ],
-        ),
+          Positioned(top: 40, left: 20, child: IconButton(icon: const Icon(Icons.arrow_back_ios), onPressed: () => Navigator.pop(context))),
+        ],
       ),
     );
   }
