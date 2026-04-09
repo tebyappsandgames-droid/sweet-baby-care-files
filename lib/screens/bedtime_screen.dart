3a35cf42c118d3a73b010257c7a99aa2cf1a653b
tebyappsandgames-droid <tebyappsandgames@gmail.com>
2026-04-09 12:42:32 -0300

    Update bedtime_screen.dart

--- lib/screens/bedtime_screen.dart
+++ lib/screens/bedtime_screen.dart
@@ -9,42 +9,43 @@ class BedtimeScreen extends StatefulWidget {
 }
 
 class _BedtimeScreenState extends State<BedtimeScreen> {
-  bool isDark = false;
-  final AudioPlayer _lullaby = AudioPlayer();
+  final AudioPlayer _lullabyPlayer = AudioPlayer();
+  bool isLightsOff = false;
 
-  void _toggleSleep() {
-    setState(() => isDark = !isDark);
-    if (isDark) {
-      _lullaby.play(AssetSource('radio_audio/tabtale_hush_little_baby.mp3'));
-    } else {
-      _lullaby.stop();
-    }
+  @override
+  void dispose() {
+    _lullabyPlayer.dispose();
+    super.dispose();
+  }
+
+  void _toggleLullaby() {
+    _lullabyPlayer.play(AssetSource('radio_audio/tabtale_hush_little_baby.mp3'));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GestureDetector(
-        onTap: _toggleSleep,
-        child: AnimatedContainer(
-          duration: const Duration(seconds: 1),
-          color: isDark ? Colors.indigo.shade900 : Colors.transparent,
-          decoration: isDark ? null : const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/backgrounds/bedroom_background.png'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Center(
+      body: Stack(
+        children: [
+          Image.asset('assets/backgrounds/bedroom_background.png', fit: BoxFit.cover, width: double.infinity, height: double.infinity),
+          if (isLightsOff) Container(color: Colors.black.withOpacity(0.6)),
+          Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image.asset('assets/characters/hope_ferrer.png', height: 400),
-                if (!isDark) const Text('Tap to Sleep', style: TextStyle(fontSize: 24, color: Colors.white)),
+                Image.asset('assets/characters/hope_ferrer.png', height: 250),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(icon: const Icon(Icons.nightlight_round, size: 50, color: Colors.yellow), onPressed: () => setState(() => isLightsOff = !isLightsOff)),
+                    IconButton(icon: const Icon(Icons.radio, size: 50, color: Colors.blue), onPressed: _toggleLullaby),
+                  ],
+                )
               ],
             ),
           ),
-        ),
+          Positioned(top: 40, left: 20, child: IconButton(icon: const Icon(Icons.arrow_back_ios), onPressed: () => Navigator.pop(context))),
+        ],
       ),
     );
   }
